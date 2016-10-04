9ef2c025ea679970b8ad14d0c768544c912d1016
Leteyski <ruslan.leteyski@save.co>
2016-10-04 22:17:57 +0200

    wtf?

--- Rainly/Rainly/CurrentWeather.swift
+++ Rainly/Rainly/CurrentWeather.swift
@@ -24,6 +24,7 @@ extension CurrentWeather {
 }
 
 extension CurrentWeather: JSONDecodable {
+        
     init?(JSON: [String: AnyObject]) {
         guard let temperature = JSON["temperature"] as? Double,
         let humidity = JSON["humidity"] as? Double,
